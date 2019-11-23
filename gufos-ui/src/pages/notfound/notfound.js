b071cba11e0765d9f7a0882b51719f93ab93d313
Saulo <saulo.macedoprojetos@gmail.com>
2019-11-23 16:58:19 -0300

    Adiciona pagina NotFound

---
+++ gufos-ui/src/pages/notfound/notfound.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div>
+                <main>
+                    <p style={{ textAlign : 'center' }}>Página não encontrada</p>
+                </main>
+            </div>
+        )
+    }
+}
+
+export default NotFound;
