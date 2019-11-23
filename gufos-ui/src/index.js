3b725c096004af0c1eddd33c89d990fcddcf717a
Saulo <saulo.macedoprojetos@gmail.com>
2019-11-22 22:24:11 -0300

    Adiciona permissao nas rotas

--- gufos-ui/src/index.js
+++ gufos-ui/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import { usuarioAutenticado, parseJwt } from './services/auth';
 
 import './index.css';
 
@@ -11,13 +12,43 @@ import Login from './pages/login/login';
 
 import * as serviceWorker from './serviceWorker';
 
+// Verifica se o usuário está logado e se é 'Administrador'
+const PermissaoAdm = ({ component : Component }) => (
+    <Route
+        render = { props => 
+            usuarioAutenticado() && parseJwt().Role === 'Administrador' ? (
+                // Se sim, renderiza de acordo com a rota solicitada e permitida
+                <Component {...props} />
+            ) : (
+                // Se não, redireciona para a página de login
+                <Redirect to = {{ pathname : 'login' }} />
+            )
+        }
+    />
+);
+
+// Verifica se o usuário está logado e se é 'Aluno'
+const PermissaoAluno = ({ component : Component }) => (
+    <Route
+        render = { props => 
+            usuarioAutenticado() && parseJwt().Role === 'Aluno' ? (
+                // Se sim, renderiza de acordo com a rota solicitada e permitida
+                <Component {...props} />
+            ) : (
+                // Se não, redireciona para a página de login
+                <Redirect to = {{ pathname : 'login' }} />
+            )
+        }
+    />
+);
+
 const routing = (
     <Router>
         <div>
             <Switch>
                 <Route exact path="/" component={App} />
-                <Route path="/categorias" component={Categorias} />
-                <Route path="/eventos" component={Eventos} />
+                <PermissaoAdm path="/categorias" component={Categorias} />
+                <PermissaoAluno path="/eventos" component={Eventos} />
                 <Route path="/login" component={Login} />
             </Switch>
         </div>
