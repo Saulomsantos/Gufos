d34fbad255ffc324777b122d2b133b302e7c1ee6
Saulo <saulo.macedoprojetos@gmail.com>
2019-11-26 23:27:58 -0300

    Adiciona editar e excluir categorias

--- gufos-ui/src/pages/categorias/categorias.js
+++ gufos-ui/src/pages/categorias/categorias.js
@@ -16,95 +16,163 @@ class Categoria extends Component {
     };
 
     // Faz a chamada para a API usando fetch
-    // e atualiza o state listaCategorias com os dados obtidos
-    // Caso ocorra algum erro, mostra no console do navegador
     buscarCategorias(){
         fetch('https://localhost:5001/api/categorias')
+
+        // Define o tipo de dado do retorno da requisição ( JSON )
         .then(resposta => resposta.json())
+
+        // e atualiza o state listaCategorias com os dados obtidos
         .then(data => this.setState({ listaCategorias : data }))
-        .catch(erro => console.log(erro));
+
+        // Caso ocorra algum erro, mostra no console do navegador
+        .catch((erro) => console.log(erro))
     };
 
-    buscarCategoriaPorId = (categoria) => {
-        this.setState({ 
-            idCategoriaAlterada : categoria.categoriaId,
-            titulo : categoria.titulo
-         }, () => {
-            console.log(categoria.categoriaId, this.state.idCategoriaAlterada)
-        });
-    }
-
-    // Chama a função buscarCategorias() assim que a tela é renderizada
+    // Chama a função buscarCategorias() assim que o componente é renderizado
     componentDidMount(){
         this.buscarCategorias();
     };
 
     // Atualiza o state titulo com o valor do input
     atualizaEstadoTitulo(event){
-        this.setState({ titulo : event.target.value });
+        this.setState({ titulo : event.target.value })
     };
 
     // Função responsável por cadastrar uma categoria
     cadastraCategoria(event){
-        
+
         // Ignora o comportamento padrão do navegador
         event.preventDefault();
 
-        fetch('https://localhost:5001/api/categorias/' + this.state.idCategoriaAlterada)
-        .then(resposta => {
-            if (resposta.data === null) {
-                // Faz a chamada para a API usando fetch
-                fetch('https://localhost:5001/api/categorias',
-                {
-                    // Define o método da requisição ( POST )
-                    method : 'POST',
-                    // Define o corpo da requisição especificando o tipo ( JSON )
-                    body : JSON.stringify({ titulo : this.state.titulo }),
-                    // Define o cabeçalho da requisição
-                    headers : {
-                        "Content-Type" : "application/json"
-                    }
-                })
+        // Caso alguma categoria seja selecionada para editar,
+        if (this.state.idCategoriaAlterada !== 0) {
 
-                // Caso a requisição retorne um status code 200,
-                // exibe no console do navegador a mensagem 'Categoria cadastrada!'
-                .then(resposta => {
-                    if (resposta.status === 200) {
-                        console.log('Categoria cadastrada!');
-                    };
-                })
+            // faz a chamada para a API usando fetch passando o ID da categoria que será atualizada na URL da requisição
+            fetch('https://localhost:5001/api/categorias/' + this.state.idCategoriaAlterada,
+            {
+                // Define o método da requisição ( PUT )
+                method : 'PUT',
 
-                // Caso ocorra algum erro,
-                // exibe este erro no console do navegador
-                .catch(erro => console.log(erro))
+                // Define o corpo da requisição especificando o tipo ( JSON )
+                body : JSON.stringify({
+                    categoriaId : this.state.idCategoriaAlterada,
+                    titulo : this.state.titulo
+                }),
 
-                // Então, atualiza a lista de categorias
-                // sem o usuário precisar executar outra ação
-                .then(this.buscarCategorias);
-            }
-            else {
-                console.log(
-                    this.state.idCategoriaAlterada,
-                    this.state.titulo
-                )
-                fetch('https://localhost:5001/api/categorias/' + this.state.idCategoriaAlterada, 
-                {
-                    method : 'PUT',
-                    body : JSON.stringify({
-                        categoriaId : this.state.idCategoriaAlterada,
-                        titulo: this.state.titulo
-                    })
-                })
-                .then(resposta => {
-                    if (resposta.status === 200) {
-                        console.log('Categoria atualizada!');
-                    };
-                })
+                // Define o cabeçalho da requisição
+                headers : {
+                    "Content-Type" : "application/json"
+                }
+            })
+
+            .then(resposta => {
+                // Caso a requisição retorne um status code 204,
+                if (resposta.status === 204) {
+                    console.log(
+                        // Exibe no console do navegador a mensagem 'Categoria x atualizada!' onde x é o ID da categoria atualizada
+                        'Categoria ' + this.state.idCategoriaAlterada + ' atualizada!',
+
+                        // e informa qual é seu novo título
+                        'Seu novo título agora é: ' + this.state.titulo
+                        );
+                };
+            });
+        };
+
+        // Caso nenhuma categoria tenha sido selecionada para editar, realiza o cadastro com a requisição abaixo
+
+        // Faz a chamada para a API usando fetch
+        fetch('https://localhost:5001/api/categorias',
+        {
+            // Define o método da requisição ( POST )
+            method : 'POST',
+
+            // Define o corpo da requisição especificando o tipo ( JSON )
+            body : JSON.stringify({ titulo : this.state.titulo }),
+
+            // Define o cabeçalho da requisição
+            headers : {
+                "Content-Type" : "application/json"
             }
         })
 
-        
+        .then(resposta => {
+            // Caso a requisição retorne um status code 200,
+            if (resposta.status === 200) {
+                // exibe no console do navegador a mensagem 'Categoria cadastrada!'
+                console.log('Categoria cadastrada!');
+            };
+        })
+
+        // Caso ocorra algum erro,
+        // exibe este erro no console do navegador
+        .catch(erro => console.log(erro))
+
+        // Então, atualiza a lista de categorias
+        // sem o usuário precisar executar outra ação
+        .then(this.buscarCategorias)
     };
+
+    // Função responsável por excluir uma categoria
+    excluirCategoria = (categoria) => {
+
+        // Exibe no console do navegador o ID da categoria recebida
+        console.log('A categoria ' + categoria.categoriaId + ' foi selecionada')
+
+        // Faz a chamada para a API usando fetch passando o ID da categoria recebida na URL da requisição
+        fetch('https://localhost:5001/api/categorias/' + categoria.categoriaId,
+        {
+            // Define o método da requisição ( DELETE )
+            method : 'DELETE',
+        })
+
+        // Caso a requisição retorne um status code 200,
+        .then(resposta => {
+            if (resposta.status === 200) {
+                // exibe no console do navegador a mensagem 'Categoria x excluída!' onde x é o ID da categoria excluída
+                console.log('Categoria ' + categoria.categoriaId + ' excluída!');
+            };
+        })
+
+        // Caso ocorra algum erro, exibe este erro no console do navegador
+        .catch(erro => console.log(erro))
+
+        // Então, atualiza a lista de categorias
+        // sem que o usuário precise executar outra ação
+        .then(this.buscarCategorias)
+    };
+
+    // Recebe uma categoria da lista
+    buscarCategoriaPorId = (categoria) => {
+        this.setState({ 
+            // Atualiza o state idCategoriaAlterada com o valor do ID da categoria recebida
+            idCategoriaAlterada : categoria.categoriaId,
+            // e o state titulo com o valor do titulo da categoria recebida
+            titulo : categoria.titulo
+         }, () => {
+             console.log(
+                // Exibe no console do navegador o valor do ID da categoria recebida,
+                'A categoria ' + categoria.categoriaId + ' foi selecionada,',
+
+                // o valor do state idCategoriaAlterada
+                'agora o valor do state idCategoriaAlterada é ' + this.state.idCategoriaAlterada,
+
+                // e o valor do state titulo
+                'e o valor do state titulo é: ' + this.state.titulo
+                );
+        });
+    };
+
+    // Reseta os states titulo e idCategoriaAlterada
+    limparCampos = () => {
+        this.setState({
+            titulo : '',
+            idCategoriaAlterada : 0
+        })
+        // Exibe no console do navegador a mensagem 'Os states foram resetados!'
+        console.log('Os states foram resetados!')
+    }
 
     render(){
         return(
@@ -125,18 +193,17 @@ class Categoria extends Component {
                             <tbody>
                                 {/* Utilizar função e map para preencher a lista */}
                                 {
-                                    // Percorre a lista e preenche o corpo da tabela com o ID 
-                                    // e o Título de cada categoria
                                     this.state.listaCategorias.map((categoria) => {
                                         return (
                                             <tr key={categoria.categoriaId}>
                                                 <td>{categoria.categoriaId}</td>
                                                 <td>{categoria.titulo}</td>
-                                                <td>
-                                                    {/* Faz a chamada para a função  */}
-                                                    <button onClick={ () => { this.buscarCategoriaPorId(categoria) } }>Editar</button>
-                                                    <button>Excluir</button>
-                                                </td>
+
+                                                {/* Faz a chamada da função buscarCategoriaPorId passando a categoria selecionada */}
+                                                <td><button onClick={() => this.buscarCategoriaPorId(categoria)}>Editar</button></td>
+
+                                                {/* Faz a chamada da função excluirCategoria passando a categoria selecionada */}
+                                                <td><button onClick={() => this.excluirCategoria(categoria)}>Excluir</button></td>
                                             </tr>
                                         );
                                     })
@@ -147,21 +214,51 @@ class Categoria extends Component {
                     <section>
                         {/* Cadastro de Categorias */}
                         <h2>Cadastro de Categorias</h2>
-                        {/* Faz a chamada para a função de cadastro quando o botão é pressionado */}
+                        
+                        {/* Formulário de cadastro de categorias */}
                         <form onSubmit={this.cadastraCategoria}>
                             <div>
                                 <input
                                     type="text"
-                                    // Define que o valor do input é o valor do state
                                     value={this.state.titulo}
-                                    // Chama a função para atualizar o state
-                                    // cada vez que há uma alteração no input
                                     onChange={this.atualizaEstadoTitulo}
                                     placeholder="Título da categoria"
                                 />
-                                <button type="submit">
-                                    Cadastrar
+
+                                {/* Altera o texto do botão de acordo com a operação ( edição ou cadastro ) */}
+
+                                {/* 
+                                    {
+                                        this.state.idCategoriaAlterada === 0 ? 
+                                        <button type="submit">Cadastrar</button> : 
+                                        <button type="submit">Editar</button>
+                                    } 
+                                */}
+
+                                {/* Uma outra forma, com if ternário */}
+
+                                {
+                                    <button type="submit">{this.state.idCategoriaAlterada === 0 ? "Cadastrar" : "Editar"}</button>
+                                }
+
+                                {/* Faz a chamada da função limparCampos */}
+                                
+                                <button type="button" onClick={this.limparCampos}>
+                                    Reset
                                 </button>
+
+                                {/* 
+                                    Caso alguma categoria tenha sido selecionada para edição,
+                                    exibe a mensagem de feedback ao usuário
+                                */}
+
+                                {
+                                    this.state.idCategoriaAlterada !== 0 &&
+                                    <div>
+                                        <p>A categoria {this.state.idCategoriaAlterada} está sendo editada </p>
+                                        <p>Clique em Reset caso queira cancelar a operação antes de cadastrar uma nova categoria</p>
+                                    </div>
+                                }                                
                             </div>
                         </form>
                     </section>
